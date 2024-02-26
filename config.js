814409f939172362c3bc8ad86f3ca2c61714960d
Raja726626 <161327701+Raja726626@users.noreply.github.com>
2024-02-26 20:31:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER || '94761516805'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
+global.owner = process.env.OWNER_NUMBER || '916289691237'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://SithumKalhara:97531@cluster0.iva7dbo.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'Sithumkalhara271@gmail.com'
@@ -15,23 +15,23 @@ global.devs = '94761516805';
 global.website = 'https://github.com/Sithuwa/SITHUWA-MD' //wa.me/+94000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/15b1dd8aeaa47888d75d7.jpg'
 module.exports = {
-  sessionName: process.env.SESSION_ID || "",      //Put Your Session Id Here
+  sessionName: process.env.SESSION_ID || "SITHUWA-MD;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMkpFVExRYkRMeTNRZjBQcGJuS2ZGbTVET2UzcWNPOWNtUWxNcGlFcXZWaz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoieE9hWThKUDRDZFMvcHk5ZHlMTmwvVW9aeFFONTlKMjcrbElKM2U0N1hocz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJnTkdDRUN4eVBTMGRMNnF3WXZ4SjZQY1ZRT0QvZWtSVWRiVlZxaloweUhjPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJodWtIWlFnYWZlZGswTmpFdG92dFN6dkVvWjUyQ1lVcDJPdFBnSHE1NUVjPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjBFRVdtSlIxSUt5cUpZRFM3bEhWV2FlSnZmUkpWT3lYcitiYXZocTl5Vms9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImlWaEtsc1p2c0pqMWQ4K0dkQisvb0hXMHo1QTRmSW80YlRRRjFhdjdsbEU9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRUE5SlBmcHJxcGprUkswV25UeUVrb2E1bHpBQ09xK1htWkVVY3FLUnYxYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoickErNFhlSU1DVEE4RVFVczhLMjd5UkE0U0V6d0JSSU5hbmxuRkRNWWtnWT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Im9sbjlzU3VPem9odDRYTkVVM3ZwT2ZJOWdaa3pHRE1LMDZjaXRYVjV0SDdhMjMwTXJHK0Fpb2lRL0ZHcUQrdGdHYlVFd0xubjBXaFVERWl4Q210U2pBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjE0LCJhZHZTZWNyZXRLZXkiOiJySWh5azlwZXBDczVycDFCMnJEQ1JIaG95cjRPckRBaFBHQUR4djkvby9NPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJPLVllQ1pNQ1NHMndfUXpzVDNfd05RIiwicGhvbmVJZCI6ImZjMjMxMjc1LWVjZGEtNGU0NS05NzVkLTcxY2JmYmVlZDcyNSIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJKQTZ2Rlo5ZjdLbHhqaVZRWkFhSlFKU0xDa3c9In0sInJlZ2lzdGVyZWQiOmZhbHNlLCJiYWNrdXBUb2tlbiI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImttWlJKVXdEdVpxVHhpZDl1YmlKaXdtczJUbz0ifSwicmVnaXN0cmF0aW9uIjp7fSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ1B5VHVZY0dFTmlxOHE0R0dBUWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6IlZkYkhWNHZKZm1xR0ZHYllDbTREcFJvdTgwL3dIMEZNOHo1VmlBQXVzaFE9IiwiYWNjb3VudFNpZ25hdHVyZSI6IlgxeTVUMDVPb3d1QmdobGhSVHBYT0tkeXpBcGFHVmNhcXdXOHFPVTBMVCtteWhWWjY2a1RMZ0gvWUlvU1g1NzRwS0V3ajRCS2dSNGNMbmhsMlQrRUR3PT0iLCJkZXZpY2VTaWduYXR1cmUiOiJxVmpkWWdCQ29ET3lZeTFwaDlPaVorQk50Q05lajJxL3FHRGVWbnNHUllMSm81dnlBNEI5a1h5aGx4aC95aDFDMzcvQndhaHcya1lqVndkRWZrWExpZz09In0sIm1lIjp7ImlkIjoiOTE2Mjg5NjkxMjM3OjE4QHMud2hhdHNhcHAubmV0IiwibmFtZSI6IkhhcnNoIn0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjkxNjI4OTY5MTIzNzoxOEBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJWWFd4MWVMeVg1cWhoUm0yQXB1QTZVYUx2TlA4QjlCVFBNK1ZZZ0FMcklVIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzA4OTU0OTc1LCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQURXTiJ9",      //Put Your Session Id Here
   author:  process.env.PACK_AUTHER ||  'SITHUWA BOT',
   packname:  process.env.PACK_NAME || 'MADE BY SITHUM KALHARA',
   
   botname:   process.env.BOT_NAME === undefined ? "SITHUWA-MD" : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'sithumkalhara' : process.env.OWNER_NAME,  
+  ownername: process.env.OWNER_NAME === undefined ? 'UCHIHA OBITO' : process.env.OWNER_NAME,  
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
-  autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
+  autoreaction:  process.env.AUTO_REACTION  === undefined ? true : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
-  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
+  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? true : process.env.ALWAYS_ONLINE,
   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '234' : process.env.FAKE_COUNTRY_CODE,
   readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
   auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
   HANDLERS:  process.env.PREFIX === undefined ? '.' : process.env.PREFIX,
   warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT,
   disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM,
-  levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE,
+  levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? true : process.env.LEVEL_UP_MESSAGE,
   antilink:  process.env.ANTILINK_VALUES === undefined ? 'chat.whatsapp.com' : process.env.ANTILINK_VALUES,
   antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION,
   BRANCH: 'main', 
